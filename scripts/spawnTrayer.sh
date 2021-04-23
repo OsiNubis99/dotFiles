1692e1af7c0526e781be3ad36f2dde2be8f43528
Andrés Hurtado <OsiNubis99@PM.me>
2021-04-23 19:04:25 -0400

    fix: Toggle trayer

--- scripts/spawnTrayer.sh
+++ scripts/spawnTrayer.sh
@@ -1,2 +1,7 @@
-kill $(pidof trayer)
-trayer --height 24 --distance 3 --tint 0x152429 --padding 3 --iconspacing 15 --edge top --align center --width 25 --alpha 128 --transparent true --expand false
+pid=$(pidof trayer || echo "0");
+if (($pid > 0))
+	then
+		kill $pid
+	else
+		trayer --height 24 --distance 3 --tint 0x152429 --padding 3 --iconspacing 15 --edge top --align center --width 25 --alpha 128 --transparent true --expand false &
+fi
