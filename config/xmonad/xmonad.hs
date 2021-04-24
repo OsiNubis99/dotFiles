71e958fe94cbee265933f172b28c92239fae1ce4
Andrés Hurtado <OsiNubis99@PM.me>
2021-04-24 00:35:46 -0400

    fix: Clean all code

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -3,7 +3,6 @@ import XMonad
 import System.IO (hPutStrLn)
 import System.Exit (exitSuccess)
 import qualified XMonad.StackSet as W
-
 -- Actions
 import XMonad.Actions.CopyWindow (kill1)
 import XMonad.Actions.CycleWS (moveTo, shiftTo, WSType(..), nextScreen, prevScreen)
@@ -13,25 +12,21 @@ import XMonad.Actions.Promote
 import XMonad.Actions.RotSlaves (rotSlavesDown, rotAllDown)
 import XMonad.Actions.WindowGo (runOrRaise)
 import XMonad.Actions.WithAll (sinkAll, killAll)
-
 -- Data
 import Data.Monoid
-
 -- Hooks
 import XMonad.Hooks.DynamicLog ( dynamicLogWithPP  , wrap, xmobarPP , xmobarColor , PP(..))
-import XMonad.Hooks.EwmhDesktops  -- for some fullscreen events, also for xcomposite in obs.
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.FadeInactive
 import XMonad.Hooks.ManageDocks (avoidStruts, docksEventHook, manageDocks, ToggleStruts(..))
 import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat)
 import XMonad.Hooks.ServerMode
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.WorkspaceHistory
-
 -- Layouts
 import XMonad.Layout.SimplestFloat
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.Tabbed
-
 -- Layouts modifiers
 import XMonad.Layout.LayoutModifier
 import XMonad.Layout.LimitWindows (limitWindows, increaseLimit, decreaseLimit)
@@ -41,32 +36,20 @@ import XMonad.Layout.MultiToggle.Instances (StdTransformers(NBFULL, MIRROR))
 import XMonad.Layout.NoBorders
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.Renamed
-import XMonad.Layout.ShowWName
+-- import XMonad.Layout.ShowWName
 import XMonad.Layout.Simplest
 import XMonad.Layout.Spacing
 import XMonad.Layout.SubLayouts
 import XMonad.Layout.WindowNavigation
-import XMonad.Layout.WindowArranger (windowArrange, WindowArrangerMsg(..))
+import XMonad.Layout.WindowArranger (windowArrange)
 import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))
 import qualified XMonad.Layout.MultiToggle as MT (Toggle(..))
-
 -- Prompt
 import XMonad.Prompt
-import XMonad.Prompt.Input
-import XMonad.Prompt.FuzzyMatch
-import XMonad.Prompt.Man
-import XMonad.Prompt.Pass
-import XMonad.Prompt.Shell
-import XMonad.Prompt.Ssh
-import XMonad.Prompt.Unicode
-import XMonad.Prompt.XMonad
-import Control.Arrow (first)
-
 -- Utilities
 import XMonad.Util.EZConfig (additionalKeysP)
-import XMonad.Util.NamedScratchpad
-import XMonad.Util.Run (runProcessWithInput, safeSpawn, spawnPipe)
-import XMonad.Util.SpawnOnce
+import XMonad.Util.Run (spawnPipe)
+import XMonad.Util.SpawnOnce (spawnOnce)
 
 myFont :: String
 myFont = "xft:Hack Nerd Font:weight=bold:pixelsize=11:antialias=true:hinting=true"
@@ -75,40 +58,36 @@ myBigFont :: String
 myBigFont = "xft:Hack Nerd Font:regular:size=73:antialias=true:hinting=true"
 
 myModMask :: KeyMask
-myModMask = mod4Mask    -- Sets modkey to super/windows key
+myModMask = mod4Mask
 
 myTerminal :: String
-myTerminal = "alacritty"  -- Sets default terminal
+myTerminal = "alacritty"
 
 myBrowser :: String
-myBrowser = "firefox"        -- Sets qutebrowser as browser for tree select
+myBrowser = "firefox"
 
 myBorderWidth :: Dimension
-myBorderWidth = 2      -- Sets border width for windows
+myBorderWidth = 2
 
 myNormColor :: String
-myNormColor  = "#152429"  -- Border color of normal windows
+myNormColor  = "#152429"
 
 myFocusColor :: String
-myFocusColor  = "#ffaa00"  -- Border color of focused windows
+myFocusColor  = "#ffaa00"
 
 altMask :: KeyMask
-altMask = mod1Mask    -- Setting this for use in xprompts
+altMask = mod1Mask
 
 windowCount :: X (Maybe String)
 windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
 
 myStartupHook :: X ()
 myStartupHook = do
-        -- spawnOnce "zsh"
-        spawnOnce "nitrogen --random --set-zoom-fill"
+        spawnOnce "~/dotFiles/scripts/setWallpaper.sh '10'"
         spawnOnce "qlipper"
         spawnOnce "picom -f"
         spawnOnce "nm-applet"
-        -- spawnOnce "flameshot"
         spawnOnce "volumeicon"
-        -- spawnOnce "~/dotFiles/scripts/spawnOnceFDM.sh"
-        -- spawnOnce "~/dotFiles/scripts/spawnOnceTelegram.sh"
         spawnOnce "ulauncher --no-window-shadow --hide-window"
         spawnOnce "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
 
@@ -131,7 +110,7 @@ otherSpacing i = spacingRaw False (Border i i i i) True (Border i i i i) False
 tall  = renamed [Replace "G"]
     $ windowNavigation
     $ addTabs shrinkText myTabTheme
-    $ subLayout [] (smartBorders Simplest)
+    $ subLayout [] (smartBorders $ tabbed shrinkText myTabTheme)
     $ magnifierOff
     $ limitWindows 12
     $ defaultSpacing 4
@@ -149,7 +128,10 @@ firstTall= tall ||| tabs
 
 firstTabs= tabs ||| tall
 
-myLayoutHook = avoidStruts $ mouseResize $ windowArrange $ T.toggleLayouts floats
+myLayoutHook = avoidStruts
+				-- $ mouseResize
+								-- $ windowArrange
+								$ T.toggleLayouts floats
         $ mkToggle (NBFULL ?? EOT) myDefaultLayout
       where
         myDefaultLayout = onWorkspaces [( myWorkspaces !! 1 )] firstTabs firstTall
@@ -189,7 +171,6 @@ myManageHook = composeAll
   , title =? "Oracle VM VirtualBox Manager"  --> doFloat
   , title =? "File Operation Progress"  --> doFloat
   , (resource =? "Dialog") --> doFloat  -- Float All Dialogs
--- , (className =? "firefox" <&&> resource =? "Dialog") --> doFloat  -- Float Firefox Dialog
   , isFullscreen --> doFullFloat
   ]
 
@@ -199,57 +180,57 @@ myLogHook = fadeInactiveLogHook fadeAmount
 
 myKeys :: [(String, X ())]
 myKeys =
-    [ ("M-c", kill1) -- Kill the currently focused client
-    , ("M-S-c", killAll) -- Kill all windows on current workspace
-    , ("M-S-q", io exitSuccess) -- Quits xmonad
-    , ("M-S-r", spawn "xmonad --restart") -- Restarts xmonad
-    , ("M-C-r", spawn "xmonad --recompile") -- Recompiles xmonad
-			-- Useful programs to have a keybinding for launch
-    , ("M-<Return>", spawn (myTerminal)) -- Launch a terminal
-    , ("M-<Space>", spawn "ulauncher-toggle") -- ulaunch
-    , ("M-x", spawn "~/dotFiles/scripts/spawnTrayer.sh") -- Toggle the trayer
-    , ("M-z", spawn (myTerminal)) -- Launch a terminal
-			-- Workspaces
-    , ("M-<Right>", moveTo Next NonEmptyWS) -- Shifts focused window to next ws
-    , ("M-<Left>", moveTo Prev NonEmptyWS) -- Shifts focused window to prev ws
-    , ("M-S-<Right>", moveTo Next EmptyWS) -- Shifts focused window to next ws
-    , ("M-S-<Left>", moveTo Prev EmptyWS) -- Shifts focused window to prev ws
-			-- Layouts
-    , ("M-<Up>", sendMessage (MT.Toggle NBFULL) >> sendMessage ToggleStruts) -- Toggles noborder/full
-    , ("M-<Down>", sendMessage NextLayout) -- Change to the next layaout
-    , ("M-b", sendMessage Toggle) -- Expand the focus windows (Magnify)
+    [ ("M-c", kill1)
+    , ("M-S-c", killAll)
+    , ("M-S-q", io exitSuccess)
+    , ("M-S-r", spawn "xmonad --restart")
+    , ("M-C-r", spawn "xmonad --recompile")
+      -- Programs
+			    , ("M-<Return>", spawn (myTerminal))
+    , ("M-<Space>", spawn "ulauncher-toggle")
+    , ("M-x", spawn "~/dotFiles/scripts/spawnTrayer.sh")
+    , ("M-z", spawn (myTerminal))
+      -- Workspaces
+    , ("M-<Right>", moveTo Next NonEmptyWS)
+    , ("M-<Left>", moveTo Prev NonEmptyWS)
+    , ("M-S-<Right>", moveTo Next EmptyWS)
+    , ("M-S-<Left>", moveTo Prev EmptyWS)
+      -- Layouts
+    , ("M-<Up>", sendMessage (MT.Toggle NBFULL) >> sendMessage ToggleStruts)
+    , ("M-<Down>", sendMessage NextLayout)
+    , ("M-b", sendMessage Toggle)
     , ("M-t", withFocused $ windows . W.sink)  -- Push floating window back to tile
-    , ("M-a", sinkAll) -- Push ALL floating windows to tile
-    , ("M-m", windows W.swapMaster) -- Move focus to the master window
-    , ("M-/", windows W.focusDown) -- Move focus to the next window
-    , ("M-,", sendMessage (IncMasterN 1)) -- Increase number of clients in master pane
-    , ("M-.", sendMessage (IncMasterN (-1))) -- Decrease number of clients in master pane
-    , ("M-d", decWindowSpacing 2) -- Decrease window spacing
-    , ("M-i", incWindowSpacing 2) -- Increase window spacing
-    , ("M-C-d", decScreenSpacing 2) -- Decrease screen spacing
-    , ("M-C-i", incScreenSpacing 2) -- Increase screen spacing
-    , ("M-S-<Up>", sendMessage MagnifyMore) -- Increase magnify window
-    , ("M-S-<Down>", sendMessage MagnifyLess) -- Decrease magnify window
-    , ("M-S-<Space>", sendMessage ToggleStruts) -- Hide bar
-    , ("M-C-<Left>", sendMessage Shrink) -- Shrink horiz window
-    , ("M-C-<Right>", sendMessage Expand) -- Expand horiz window
-    , ("M-C-<Up>", sendMessage MirrorExpand) -- Expand vert window
-    , ("M-C-<Down>", sendMessage MirrorShrink) -- Shrink vert window
-			-- Wallpapers
-    , ("M-w o", spawn "nitrogen") -- Set a random wallpaper
-    , ("M-w a", spawn "~/dotFiles/scripts/setWallpaper.sh 0") -- Set a random wallpaper
-    , ("M-w f", spawn "nitrogen --restore") -- Set a default wallpaper
-    , ("M-w 1", spawn "~/dotFiles/scripts/setWallpaper.sh 1") -- Set a random wallpaper from folder
-    , ("M-w 2", spawn "~/dotFiles/scripts/setWallpaper.sh 2") -- Set a random wallpaper from folder
-    , ("M-w 3", spawn "~/dotFiles/scripts/setWallpaper.sh 3") -- Set a random wallpaper from folder
-    , ("M-w 4", spawn "~/dotFiles/scripts/setWallpaper.sh 4") -- Set a random wallpaper from folder
-    , ("M-w 5", spawn "~/dotFiles/scripts/setWallpaper.sh 5") -- Set a random wallpaper from folder
-    , ("M-w 6", spawn "~/dotFiles/scripts/setWallpaper.sh 6") -- Set a random wallpaper from folder
-    , ("M-w 7", spawn "~/dotFiles/scripts/setWallpaper.sh 7") -- Set a random wallpaper from folder
-    , ("M-w 8", spawn "~/dotFiles/scripts/setWallpaper.sh 8") -- Set a random wallpaper from folder
-    , ("M-w 9", spawn "~/dotFiles/scripts/setWallpaper.sh 9") -- Set a random wallpaper from folder
-    , ("M-w 10", spawn "~/dotFiles/scripts/setWallpaper.sh 10") -- Set a random wallpaper from folder
-			-- Sub layouts
+    , ("M-a", sinkAll)
+    , ("M-m", windows W.swapMaster)
+    , ("M-/", windows W.focusDown)
+    , ("M-,", sendMessage (IncMasterN 1))
+    , ("M-.", sendMessage (IncMasterN (-1)))
+    , ("M-d", decWindowSpacing 2)
+    , ("M-i", incWindowSpacing 2)
+    , ("M-C-d", decScreenSpacing 2)
+    , ("M-C-i", incScreenSpacing 2)
+    , ("M-S-<Up>", sendMessage MagnifyMore)
+    , ("M-S-<Down>", sendMessage MagnifyLess)
+    , ("M-S-<Space>", sendMessage ToggleStruts)
+    , ("M-C-<Left>", sendMessage Shrink)
+    , ("M-C-<Right>", sendMessage Expand)
+    , ("M-C-<Up>", sendMessage MirrorExpand)
+    , ("M-C-<Down>", sendMessage MirrorShrink)
+      -- Wallpapers
+    , ("M-w o", spawn "nitrogen")
+    , ("M-w a", spawn "~/dotFiles/scripts/setWallpaper.sh '10'")
+    , ("M-w f", spawn "nitrogen --restore")
+    , ("M-w 1", spawn "~/dotFiles/scripts/setWallpaper.sh '0'")
+    , ("M-w 2", spawn "~/dotFiles/scripts/setWallpaper.sh '1'")
+    , ("M-w 3", spawn "~/dotFiles/scripts/setWallpaper.sh '2'")
+    , ("M-w 4", spawn "~/dotFiles/scripts/setWallpaper.sh '3'")
+    , ("M-w 5", spawn "~/dotFiles/scripts/setWallpaper.sh '4'")
+    , ("M-w 6", spawn "~/dotFiles/scripts/setWallpaper.sh '5'")
+    , ("M-w 7", spawn "~/dotFiles/scripts/setWallpaper.sh '6'")
+    , ("M-w 8", spawn "~/dotFiles/scripts/setWallpaper.sh '7'")
+    , ("M-w 9", spawn "~/dotFiles/scripts/setWallpaper.sh '8'")
+    , ("M-w 0", spawn "~/dotFiles/scripts/setWallpaper.sh '9'")
+      -- Sub layouts
     , ("M-s ;", sendMessage $ pullGroup L)
     , ("M-s ]", sendMessage $ pullGroup R)
     , ("M-s [", sendMessage $ pullGroup U)
@@ -257,16 +238,7 @@ myKeys =
     , ("M-s m", withFocused (sendMessage . MergeAll))
     , ("M-s u", withFocused (sendMessage . UnMerge))
     , ("M-s a", withFocused (sendMessage . UnMergeAll))
-
-			-- Windows navigation
-    , ("M-k", windows W.focusUp)    -- Move focus to the prev window
-    , ("M-S-j", windows W.swapDown)  -- Swap focused window with next window
-    , ("M-S-k", windows W.swapUp)  -- Swap focused window with prev window
-    , ("M-S-<Backspace>", promote)    -- Moves focused window to master, others maintain order
-    , ("M-S-<Tab>", rotSlavesDown)  -- Rotate all windows except master and keep focus in place
-    , ("M-C-<Tab>", rotAllDown)    -- Rotate all the windows in the current stack
-
-			-- Multimedia Keys
+      -- Multimedia Keys
     , ("<XF86AudioLowerVolume>", spawn "amixer set Master 5%- unmute")
     , ("<XF86AudioRaiseVolume>", spawn "amixer set Master 5%+ unmute")
     , ("<XF86AudioMute>",  spawn "amixer set Master toggle")
@@ -274,13 +246,14 @@ myKeys =
     , ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 20")
     , ("<XF86HomePage>", spawn (myBrowser))
     , ("<Print>", spawn "flameshot gui")
-    -- , ("<XF86AudioNext>", spawn (myTerminal ++ "mocp --next"))
-    -- , ("<XF86AudioPlay>", spawn (myTerminal ++ "mocp --play"))
-    -- , ("<XF86AudioPrev>", spawn (myTerminal ++ "mocp --previous"))
-    -- , ("<XF86Mail>", runOrRaise "thunderbird" (resource =? "thunderbird"))
-    -- , ("<XF86Search>", safeSpawn (myBrowser) ["https://www.duckduckgo.com/"])
-    -- , ("<XF86Calculator>", runOrRaise "qalculate-gtk" (resource =? "qalculate-gtk"))
-    -- , ("<XF86Eject>", spawn "toggleeject")
+
+      -- Windows navigation
+    , ("M-k", windows W.focusUp)    -- Move focus to the prev window
+    , ("M-S-j", windows W.swapDown)  -- Swap focused window with next window
+    , ("M-S-k", windows W.swapUp)  -- Swap focused window with prev window
+    , ("M-S-<Backspace>", promote)    -- Moves focused window to master, others maintain order
+    , ("M-S-<Tab>", rotSlavesDown)  -- Rotate all windows except master and keep focus in place
+    , ("M-C-<Tab>", rotAllDown)    -- Rotate all the windows in the current stack
     ]
 
 main :: IO ()
