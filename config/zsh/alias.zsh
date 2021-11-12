bb8deb1a9a830e45319c4d338a9e8d216b45af59
Andrés David Hurtado Fernández <OsiNubis99@PM.me>
2021-11-11 21:28:23 -0400

    fix: Reorder and created new aliases

--- config/zsh/alias.zsh
+++ config/zsh/alias.zsh
@@ -1,26 +1,6 @@
 # Node
 alias nr="npm run"
 alias ni="npm i"
-# Productivity
-alias cp="cp -i"                                                
-alias mv="mv -i"
-alias df="df -h"
-alias rm="rm -r"
-alias lsl="ls -l"
-alias free="free -m"
-alias srm="doas rm -r"
-alias mkdir="mkdir -p"
-alias ls="ls --color=auto -A"
-alias gitu="git add . && git commit && git push"
-# vim and emacs
-alias v="doas nvim"
-alias em="/usr/bin/emacs -nw"
-alias emacs="emacsclient -c -a 'emacs'"
-alias doomsync="~/.emacs.d/bin/doom sync"
-alias doomdoctor="~/.emacs.d/bin/doom doctor"
-alias doomupgrade="~/.emacs.d/bin/doom upgrade"
-alias doompurge="~/.emacs.d/bin/doom purge"
-
 # Pacman
 alias pain="paru -S --removemake --cleanafter --skipreview"
 alias pais="paru -Ss"
@@ -30,6 +10,31 @@ alias pau="pai -yyu --upgrademenu"
 alias par="paru -Rcnsu"
 alias pale="paru -Qqe"
 alias pald="paru -Qqd"
+# Productivity
+alias cd="cdl"
+alias cp="cp -r"
+alias mv="mv -r"
+alias df="df -h"
+alias rm="rm -r"
+alias lsl="ls -l"
+alias free="free -m"
+alias srm="doas rm -r"
+alias mkdir="mkdir -p"
+alias reboot="doas reboot"
+alias ls="ls --color=auto -A"
+alias sd="fd -i -c auto -t d"
+alias sf="fd -i -c auto -t f"
+alias gitu="git add . && git commit && git push"
+# vim and emacs
+alias vi="nvim"
+alias vim="nvim"
+alias v="doas nvim"
+alias em="/usr/bin/emacs -nw"
+alias emacs="emacsclient -c -a 'emacs'"
+alias doomsync="~/.emacs.d/bin/doom sync"
+alias doomdoctor="~/.emacs.d/bin/doom doctor"
+alias doomupgrade="~/.emacs.d/bin/doom upgrade"
+alias doompurge="~/.emacs.d/bin/doom purge"
 # Functions
 cdl ()
 {
