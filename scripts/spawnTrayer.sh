e2c6edcfd3fa1ce20fbd74021e493a56f21c7f1d
Andrés David Hurtado Fernández <AHurtado.WC@gmail.com>
2022-03-30 13:38:11 -0400

    tag: Font size

--- scripts/spawnTrayer.sh
+++ scripts/spawnTrayer.sh
@@ -3,5 +3,5 @@ if (($pid > 0))
 	then
 		kill $pid
 else
-	trayer --height 24 --distance 3 --tint 0x000000 --padding 3 --iconspacing 15 --edge top --align center --width 21 --alpha 95 --transparent true --expand false &
+	trayer --height 24 --distance 3 --tint 0x000000 --padding 3 --iconspacing 15 --edge top --align center --width 19 --alpha 95 --transparent true --expand false &
 fi
