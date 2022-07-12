b7d25c7add0d9958caff096ce70d1f37f3352a98
Andrés David Hurtado Fernández <AHurtado.WC@gmail.com>
2022-07-12 11:16:49 -0400

    feat: Install script v2.0

--- install.sh
+++ install.sh
@@ -3,9 +3,31 @@
 ## Clean temp file
 echo "" > ~/dotFiles/app.temp
 
-# Nedded
+## Read flags
+ASK=false
+while getopts 'yh' OPTION; do
+	case "$OPTION" in
+		h)
+			echo "Fast install script"
+			printf "\n\t-y Avoid confirm every step"
+			printf "\n\t-h Print this message\n"
+			exit 0
+			;;
+		y)
+			ASK=true
+			REPLY="Y"
+			;;
+		?)
+			echo "Use with -y to avoid confirm every step"
+			exit 1
+			;;
+	esac
+done
+
+# Welcome
 echo "Welcome to the Instalation Script"
 echo "Create folders and copy globals settings"
+
 ## Creating Folders
 mkdir -p ~/.config
 mkdir -p ~/.local/share
@@ -15,20 +37,28 @@ mkdir -p ~/Music
 mkdir -p ~/Pictures
 mkdir -p ~/Public
 mkdir -p ~/Videos
+
 ## ETC configs
 sudo cp -r ~/dotFiles/config/etc /
+
 ## Fontconfig
 rm -r -f ~/.local/share/fonts
 ln -s ~/dotFiles/config/fonts ~/.local/share/fonts
 rm -r -f ~/.config/fontconfig
 ln -s ~/dotFiles/config/fontconfig ~/.config/fontconfig
+
 ## Git
+if [[ ! $(which git 2> /dev/null) ]]
+then
+  sudo pacman -Syu --needed --noconfirm git
+fi
 rm -r -f ~/.gitconfig
 ln -s ~/dotFiles/config/git/gitconfig ~/.gitconfig
-echo -n "Do you have your backup folder?(Wakatime and Wallpapers)[Y/n]" && read
+
+## SSH
+$ASK || ( echo -n "Do you have your backup folder? (Wakatime and Wallpapers) [Y/n]" && read )
 if [[ ! $REPLY =~ ^[Nn]$ ]]
 then
-	## SSH
 	rm -r -f ~/.ssh
 	mkdir ~/.ssh
 	cp ~/dotFiles/backup/ssh/* ~/.ssh/
@@ -36,7 +66,7 @@ then
 		chmod -R 700 ~/.ssh
 		eval "$(ssh-agent -s)"
 		ssh-add ~/.ssh/id_ed25519
-		echo "shh key added"
+		echo "ssh key added"
 	fi
 	## Wakatime
 	rm -r -f ~/.wakatime.cfg
@@ -45,27 +75,46 @@ then
 	rm -r -f ~/Pictures/wallpapers
 	ln -s ~/dotFiles/backup/wallpapers ~/Pictures/wallpapers
 fi
+
 ## ZSH
 rm -r -f ~/.zshrc
 ln -s ~/dotFiles/config/zsh/.zshrc ~/.zshrc
-# Opcional
+
+## Develop
+$ASK || ( echo -n "Do you need all develop software? [Y/n]" && read )
+if [[ ! $REPLY =~ ^[Nn]$ ]]
+then
+	cat ~/dotFiles/apps/development >> ~/dotFiles/app.temp
+fi
+
+## Doom
+$ASK || ( echo -n "Do you need Doom Emacs? [Y/n]" && read )
+if [[ ! $REPLY =~ ^[Nn]$ ]]
+then
+  if [[ ! $(~/.emacs.d/bin/doom info 2> /dev/null) ]]
+  then
+ 	  git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d 1> /dev/null || exit 1
+	  ~/.emacs.d/bin/doom install --install --no-fonts 1> /dev/null || exit 1
+  fi
+	cat ~/dotFiles/apps/emacs >> ~/dotFiles/app.temp
+fi
+
+## Files
+$ASK || ( echo -n "Do you need file system software? [Y/n]" && read )
+if [[ ! $REPLY =~ ^[Nn]$ ]]
+then
+	cat ~/dotFiles/apps/files >> ~/dotFiles/app.temp
+fi
+
 ## Games
-echo -n "Do you need all gaiming software? [Y/n]" && read
+$ASK || ( echo -n "Do you need all gaiming software? [Y/n]" && read )
 if [[ ! $REPLY =~ ^[Nn]$ ]]
 then
-	cat ~/dotFiles/apps/game/amd >> ~/dotFiles/app.temp
+	cat ~/dotFiles/apps/games >> ~/dotFiles/app.temp
 fi
-## Others
-echo "Run"
-echo "	cat ~/dotFiles/apps/others"
-echo "for information about what other apps will be installed"
-echo -n "Do you this other apps? [Y/n]" && read
-if [[ ! $REPLY =~ ^[Nn]$ ]]
-then
-	cat ~/dotFiles/apps/others >> ~/dotFiles/app.temp
-fi
+
 ## Xmonad
-echo -n "Do you want Xmonad? [Y/n]" && read
+$ASK || ( echo -n "Do you want Xmonad? [Y/n]" && read )
 if [[ ! $REPLY =~ ^[Nn]$ ]]
 then
 	echo "Copying all desktop settings..."
@@ -96,27 +145,34 @@ then
 	echo "--- Xmonad"
 	rm -r -f ~/.xmonad
 	ln -s ~/dotFiles/config/xmonad ~/.xmonad
-### Add programs
+  ### Add programs
 	cat ~/dotFiles/apps/xmonad >> ~/dotFiles/app.temp
 fi
+
+## Others
+echo "Run"
+echo "	cat ~/dotFiles/apps/others"
+echo "for information about what other apps will be installed"
+$ASK || ( echo -n "Do you this other apps? [Y/n]" && read )
+if [[ ! $REPLY =~ ^[Nn]$ ]]
+then
+	cat ~/dotFiles/apps/others >> ~/dotFiles/app.temp
+fi
+
+## Install paru only if it doesn't exist in path
+if [[ ! $(which paru 2> /dev/null) ]]
+then
+	sudo rm -r /home/andres/.cache/paru
+	git clone https://aur.archlinux.org/paru.git ~/.cache/paru
+	cd ~/.cache/paru/ || exit 1
+	makepkg -si
+	cd ~/ || exit 1
+	sudo rm -r /home/andres/.cache/paru
+fi
+
 # Final instalation
 echo "Inastalling all nedeed software..."
-sudo pacman -Syu --needed --noconfirm $( cat ~/dotFiles/apps/default ) $( cat ~/dotFiles/app.temp )
-## Paru
-echo -n "Do you need Paru and all Paru software? [Y/n]" && read
-if [[ ! $REPLY =~ ^[Nn]$ ]]
-then
-	git clone https://aur.archlinux.org/paru.git ~/.cache/paru
-	~/.cache/paru/makepkg -si
-	paru -S --needed --noconfirm $( cat ~/dotFiles/apps/aur )
-fi
-## Doom
-echo -n "Do you need Doom Emacs? [Y/n]" && read
-if [[ ! $REPLY =~ ^[Nn]$ ]]
-then
-	git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
-	~/.emacs.d/bin/doom install
-	paru -S --needed --noconfirm $( cat ~/dotFiles/apps/emacs )
-fi
+sudo paru -Syu --needed --noconfirm $( cat ~/dotFiles/apps/system ) $( cat ~/dotFiles/app.temp ) 1> /dev/null || exit 1
+
 rm -r ~/dotFiles/app.temp
 echo "Instalation Complete!"
